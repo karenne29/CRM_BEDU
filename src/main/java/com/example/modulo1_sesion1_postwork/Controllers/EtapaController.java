7c024771eab1e557a9758186c10f6ab709460998
karenne29 <56751691+karenne29@users.noreply.github.com>
2022-09-12 12:34:42 -0500

    Update EtapaController.java
    Modificacion a la respuesta del delete

--- src/main/java/com/example/modulo1_sesion1_postwork/Controllers/EtapaController.java
+++ src/main/java/com/example/modulo1_sesion1_postwork/Controllers/EtapaController.java
@@ -1,8 +1,6 @@
 package com.example.modulo1_sesion1_postwork.Controllers;
 
-import com.example.modulo1_sesion1_postwork.model.ClienteModel;
 import com.example.modulo1_sesion1_postwork.model.EtapaModel;
-import com.example.modulo1_sesion1_postwork.services.ClienteService;
 import com.example.modulo1_sesion1_postwork.services.EtapaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,43 +20,43 @@ public class EtapaController {
     private final EtapaService etapaService;
 
     @GetMapping("/{etapaId}")
-    public ResponseEntity<EtapaModel> getCliente(@PathVariable Long etapaId){
+    public ResponseEntity<EtapaModel> getEtapa(@PathVariable Long etapaId){
         Optional<EtapaModel> etapaDb = etapaService.obtenEtapa(etapaId);
 
         if (etapaDb.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El cliente especificado no existe.");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La etapa especificado no existe.");
         }
 
         return ResponseEntity.ok(etapaDb.get());
     }
 
-    @GetMapping("/getAllClientes")
-    public ResponseEntity <List<EtapaModel>> getClientes(){
+    @GetMapping("/getAllEtapas")
+    public ResponseEntity <List<EtapaModel>> getEtapas(){
 
         return ResponseEntity.ok(etapaService.obtenEtapas());
     }
 
-    @PostMapping("/addCliente")
-    public ResponseEntity<String> creaCliente(@Valid @RequestBody EtapaModel etapaModel){
+    @PostMapping("/addEtapa")
+    public ResponseEntity<String> creaEtapa(@Valid @RequestBody EtapaModel etapaModel){
         etapaService.guardaEtapa(etapaModel);
-        return ResponseEntity.status(201).body("Se agrego el cliente");
+        return ResponseEntity.status(201).body("Se agrego la etapa");
     }
 
-    @PutMapping("/edit/{clienteId}")
-    public ResponseEntity<String> actualizaCliente(@PathVariable Long etapaId,@Valid @RequestBody  EtapaModel etapaModel){
+    @PutMapping("/edit/{etapaId}")
+    public ResponseEntity<String> actualizaEtapa(@Valid @PathVariable Long etapaId, @RequestBody  EtapaModel etapaModel){
         if(etapaService.obtenEtapa(etapaId).get()!=null){
             etapaModel.setEtapaId(etapaId);
             etapaService.actualizaEtapa(etapaModel);
-            return ResponseEntity.status(201).body("Se actualizo la informacion del cliente");
+            return ResponseEntity.status(201).body("Se actualizo la informacion de la etapa");
         }else{
             return ResponseEntity.status(201).body("El Id que ingresaste no existe");
         }
 
     }
 
-    @DeleteMapping("/delete/{clienteId}")
-    public ResponseEntity<String> eliminaCliente(@PathVariable Long clienteId){
-        etapaService.eliminaEtapa(clienteId);
-        return ResponseEntity.status(201).body("Se elimino correctamente el cliente");
+    @DeleteMapping("/delete/{etapaId}")
+    public ResponseEntity<String> eliminaEtapa(@Valid @PathVariable Long etapaId){
+        etapaService.eliminaEtapa(etapaId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
